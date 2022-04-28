7e4537a418ae3dfcd432f93e1f758d6ffdce48f8
grouning <slip731@gmail.com>
2022-04-28 03:04:43 +0300

    Lesson 3

---
+++ Lesson3.py
@@ -0,0 +1,15 @@
+# Задача 1.
+# def my_func (x, y):
+#     try:
+#         z = x / y
+#         return z
+#     except ZeroDivisionError:
+#         return "На ноль делить нельзя!"
+# print(my_func(int(input("Введите первое число: ")), int(input("Введите второе число: "))))
+
+# Задача 2.
+# def my_func(firstname, lastname, birthday, city, email, phone):
+#     print(firstname, lastname, birthday, city, email, phone)
+# my_func(input('Введите имя: '), input('Введите фамилию: '), input('Введите дату рождения: '), input('Введите город проживания: '), input('Введите email: '), input('Введите номер телефона: '))
+
+# Задача 3.
